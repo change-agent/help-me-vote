796efb98ff17a8f201f11f3bce83cff138b05592
change-agent <dan@ohmdee.com>
2024-09-09 00:56:31 +1000

    Add build folder for GitHub Pages deployment

---
+++ build/precache-manifest.5c06eaefca405fa7cf46f581225789b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae50bec8e12965742986c024328bdf0a",
+    "url": "/help-me-vote/index.html"
+  },
+  {
+    "revision": "350fdb98cb7e881c4f5b",
+    "url": "/help-me-vote/static/css/main.8c2c626c.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/help-me-vote/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "350fdb98cb7e881c4f5b",
+    "url": "/help-me-vote/static/js/main.6112367a.chunk.js"
+  },
+  {
+    "revision": "67b1be67d0977d056692",
+    "url": "/help-me-vote/static/js/runtime~main.4a5bc2da.js"
+  }
+]);
