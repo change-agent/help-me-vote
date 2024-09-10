cba089dfb11a179f677c9518dea44e67276eaed4
change-agent <dan@ohmdee.com>
2024-09-10 20:06:14 +1000

    Deploy build to GitHub Pages

---
+++ build/precache-manifest.d574a37091e2eecf4f826f418b3611b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7331331fd31f9c326b633b2207331b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd9b1abd6b48b6309b0",
+    "url": "/static/css/main.d709b6bf.chunk.css"
+  },
+  {
+    "revision": "7d321d456096e9459b03",
+    "url": "/static/js/2.41d23a0f.chunk.js"
+  },
+  {
+    "revision": "fbd9b1abd6b48b6309b0",
+    "url": "/static/js/main.b3d69341.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f8b140f58b80f6bfe71f2865067817c",
+    "url": "/static/media/VoteSmartLogo.9f8b140f.svg"
+  }
+]);
